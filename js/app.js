c4103bbb4f9a763de80dcf27a79cf80a400af0b3
Juan Pablo Eslava <jpesmar@gmail.com>
2018-04-27 23:16:30 +0200

    feat: fucked up initial shuffling. It doesn't show the cards.

--- js/app.js
+++ js/app.js
@@ -1,22 +1,27 @@
 /*
 ON 'master' BRANCH
 */
-
+console.log('starting now');
+/* global variables */
+// var containing all classes
+let allCards = [
+	'fa fa-diamond', 'fa fa-diamond',
+	'fa fa-paper-plane-o', 'fa fa-paper-plane-o',
+	'fa fa-anchor', 'fa fa-anchor',
+	'fa fa-bolt', 'fa fa-bolt',
+	'fa fa-cube', 'fa fa-cube',
+	'fa fa-bomb', 'fa fa-bomb',
+	'fa fa-leaf', 'fa fa-leaf',
+	'fa fa-bicycle', 'fa fa-bicycle'
+];
+// from DOM
+let deck = document.querySelector('.deck');
+// my variables
+console.log('this is how the normal deck looks:');
+console.log(deck);
 /*
  * Create a list that holds all of your cards
  */
- // array containing all fa classes that'll be displayed as cards later on
-let allCards = [
-	'fa-diamond', 'fa-diamond',
-	'fa-paper-plane-o', 'fa-paper-plane-o',
-	'fa-anchor', 'fa-anchor',
-	'fa-bolt', 'fa-bolt',
-	'fa-cube', 'fa-cube',
-	'fa-bomb', 'fa-bomb',
-	'fa-leaf', 'fa-leaf',
-	'fa-bicycle', 'fa-bicycle'
-];
-
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
@@ -24,21 +29,50 @@ let allCards = [
  *   - add each card's HTML to the page
  */
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-// Apparently it as simple as using it without questioning why the fuck does this works?
-function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+// FUNCTION: Initial setup for page load
+function firstSetUp () {
+	// Erase current cards inside deck
+	deck.innerHTML = '';
+	let shuffledCards = shuffle(allCards);
+	/* 
+	the total amount of items created should be the total number of items contained in shuffledCards (d.H shuffleCards.length).
+	For each item, it should create a <li> item with a 
+	*/
+	// draw the empty cards within the container
+	for (let listElement = 0; listElement < allCards.length; listElement++) {
+		// for eavery iteration of the loop it'll add a list element to the empty deck
+		deck.innerHTML += 
+						'<li class="card">' + 
+							'<i class=""></i>' +
+						'</li>';
+	}
+	// store each newly created empty card into a variable
+	let cardItems = document.querySelectorAll('.card');
+	// give each empty card a class from the shuffled list
+	for (let i = 0; i < allCards.length; i++) {
+		cardItems[i].className = shuffledCards[i];
+	}
 
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
+	// FUNCTION: Shuffle function from http://stackoverflow.com/a/2450976
+	function shuffle(array) {
+	    var currentIndex = array.length, temporaryValue, randomIndex;
+	    // this does something
+	    while (currentIndex !== 0) {
+	        randomIndex = Math.floor(Math.random() * currentIndex);
+	        currentIndex -= 1;
+	        temporaryValue = array[currentIndex];
+	        array[currentIndex] = array[randomIndex];
+	        array[randomIndex] = temporaryValue;
+	    }
+	    // this returns something too
+	    return array;
+	}
 
-    return array;
+
+	console.log('now the deck looks like this:');
+	console.log(deck);
 }
+
 
 /*
  * set up the event listener for a card. If a card is clicked:
